0ac1804822dbc0476fe5113e294433423af11530
ezid007 <ezid007@example.com>
2025-11-26 12:38:32 +0900

    우선 로컬에서만 작동하도록 수정

--- main.py
+++ main.py
@@ -404,4 +404,4 @@ async def read_contact(request: Request):
 
 if __name__ == "__main__":
     # 로컬 테스트용 (8000번 포트)
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
